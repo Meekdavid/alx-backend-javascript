31ffc9fedd01edb4b10484800270bc83c0a24626
Mboko David <mbokodavid@gmail.com>
2023-01-25 22:26:38 +0100

    APIs Endpoint Developemnt

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,12 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else {
+      reject(new Error('The API is not working fine'));
+    }
+  });
+}
---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,6 @@
+export default function handleResponseFromAPI(promise) {
+  return promise
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('There was an response from the API'));
+}
